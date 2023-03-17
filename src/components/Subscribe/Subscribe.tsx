be2d14e7a6a13f1c1ad71730e8f59787daca0f05
Olawale Shopeyin <shopeyinwale@gmail.com>
2023-03-17 14:50:20 +0100

    Subscribe component

--- src/components/Subscribe/Subscribe.tsx
+++ src/components/Subscribe/Subscribe.tsx
@@ -1,7 +1,27 @@
+import { Space } from "@mantine/core";
 import "./subscribe.scss";
 
 const Subscribe = () => {
-  return <div>lorem2900</div>;
+  return (
+    <div className="subscribe">
+      <div className="inner-subscribe">
+        <h1>Subscribe to our Newsletter</h1>
+        <Space h="md" />
+        <p>Get daily news directly to your mail to always keep you updated.</p>
+        <Space h="md" />
+        <div className="input-box">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter your email address"
+            className="input"
+          />
+          <button type="submit">Submit</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Subscribe;
